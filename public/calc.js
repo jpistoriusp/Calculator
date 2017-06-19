fb81d529b0805f4db4f898b5d2611359de6d6b26
Paul Pistorius <jpistoriusp@gmail.com>
2017-06-18 23:10:38 -0600

    working clear

--- public/calc.js
+++ public/calc.js
@@ -1,20 +1,19 @@
-function start() {
+
+$(document).ready(function() {
+  console.log('test');
   $('number').on('click', function(e) {
     e.preventDefault();
-     $('#clear');
-      console.log('ready');
-      console.log('test');
-
+    //  $('#clear');
+     console.log($('number').text());
+     console.log('hi');
   });
-}
-$(document).ready(function() {
-
-  start();
-
-  console.log('test');
-
+  $('#clear').on('click', function(e) {
+    $('#display').text('');
+  });
 });
+
 var calc = function(oper, value1, value2) {
+  var total = 0;
   switch (oper) {
     case '+': total = value1 + value2;
       break;
@@ -26,4 +25,5 @@ var calc = function(oper, value1, value2) {
       break;
     default:
   }
+  $('#display').text(total)
 }
